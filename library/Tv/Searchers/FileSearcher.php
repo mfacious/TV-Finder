de81eb0aece6be078f7de89787d18521a176d46a
Thomas Gray <tom@osg.me>
2012-03-25 18:32:58 +0100

    Adding pattern match & recursive searcher to FileSearcher class

--- library/Tv/Searchers/FileSearcher.php
+++ library/Tv/Searchers/FileSearcher.php
@@ -4,8 +4,19 @@ namespace Tv\Searchers;
 
 class FileSearcher implements SearcherInterface
 {
+
+    /**
+     *
+     * @var string $downloadDirectory 
+     */
     protected $_downloadDir;
     
+    /**
+     *
+     * @var \RecursiveDirectoryIterator $iterator 
+     */
+    protected $_directoryIterator;
+
     public function getDownloadDir()
     {
         return $this->_downloadDir;
@@ -15,66 +26,107 @@ class FileSearcher implements SearcherInterface
     {
         $this->_downloadDir = $downloadDir;
     }
-       
+
+    public function getDirectoryIterator()
+    {
+        if (!$this->_directoryIterator instanceof \RecursiveDirectoryIterator) {
+            throw new \DomainException(__METHOD__ . ' could not find an'
+                    . ' instance of the RecursiveDirectoryIterator. Perhaps'
+                    . ' you haven\'t called the ' . __CLASS__ . '::'
+                    . 'setupDirectoryIterator() method?');
+        }
+
+        return $this->_directoryIterator;
+    }
+
+    public function setDirectoryIterator($directoryIterator)
+    {
+        $this->_directoryIterator = $directoryIterator;
+    }
+
     public function __construct($downloadDir)
     {
-        if(empty($downloadDir))
-        {
+        if (empty($downloadDir)) {
             throw new \InvalidArgumentException(__CLASS__ . ' requires a download'
                     . ' directory to search in as a constructor argument.');
         }
-        
-        if(!is_string($downloadDir))
-        {
+
+        if (!is_string($downloadDir)) {
             throw new \InvalidArgumentException(__CLASS__ . ' download dir passed'
                     . ' must be a string!');
         }
-        
+
         $this->setDownloadDir($downloadDir);
     }
-    
+
     public function resetSearch()
     {
+        $this->setupDirectoryIterator();
         return $this;
     }
-    
+
     protected function setupRegex(array $names)
     {
         $rules = array();
-        foreach($names as $name)
-        {
+        foreach ($names as $name) {
             $nameParts = explode(" ", $name);
             $regex = "/" . implode('.*', $nameParts) . ".*S([0-9]{1,2}).*E([0-9]{1,2})/i";
-            
+
             $rules[$name] = $regex;
         }
-        
+
+        if (empty($rules)) {
+            throw new \OutOfRangeException(__METHOD__ . ' Could not generate'
+                    . ' any patterns to use to find shows!');
+        }
+
         return $rules;
     }
-    
+
+    protected function setupDirectoryIterator()
+    {
+        $directoryIterator = new \RecursiveDirectoryIterator(
+                        $this->getDownloadDir()
+        );
+
+        $this->setDirectoryIterator($directoryIterator);
+    }
+
     public function find($name, array $aliases)
     {
+        // Generate an array of all our names and alaises we want to match
+        // for this show.
         $names = array_merge($aliases, array($name));
-        
+
+        // Get an array of all the regex patterns we will use.
         $showRegexes = $this->setupRegex($names);
         
-        die();
+        // array that will contain all of the matched episodes we already have.
+        $episodes = array();
         
-        $eps = array();
-        $directoryIterator = new \RecursiveDirectoryIterator(
-                $this->getDownloadDir()
-        );
-        
-        
-        var_dump($name, $aliases, $regex);
-        die();
-        
-        foreach (new RecursiveIteratorIterator($directoryIterator) as $filename => $cur) {
-            if(preg_match($regex,$filename,$matches) > 0){
-                $eps[$matches[1]][$matches[2]] = true;
+
+        // Cycle through each of those ptterns and try to find shows.
+        foreach ($showRegexes as $pattern) {
+            
+            $directoryIterator = $this->getDirectoryIterator();
+
+            
+            foreach (
+                new \RecursiveIteratorIterator($directoryIterator) as $path) {
+                
+                $fileName = $path->getFilename();
+                
+                if (preg_match($pattern, $fileName, $matches) > 0) {
+                    $episodes[] = array(
+                        'series' => $matches[1],
+                        'episode' => $matches[2],
+                    );
+                }
             }
         }
+        
+        return $episodes;
+
     }
-    
-    
+
 }
